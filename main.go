7a303afa06501d40be7d360a9992769dc5143586
Wagner Fonseca <wagner.fonseca@gmail.com>
2026-09-15 16:45:23 -0300

    Add logging for server start and handle fatal errors

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+)
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Full Cycle Rocks!!!"))
 	})
-	http.ListenAndServe(":8080", nil)
+	log.Println("Server is running on port 8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
